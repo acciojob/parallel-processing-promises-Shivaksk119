e350d31900290307a668c081d07065f259544d87
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-19 23:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const loadImage = (url)=>{
+	return new Promise(resolve, reject){
+		let img = new Image();
+		img.src = url;
+		if(img.onload()){
+			resolve(img);
+		}
+		else {
+			reject(url);
+		}
+	}
+}
+
+const displayImages=(images)=>{
+	let imagePromises = images.map(loadImage);
+
+	Promise.All(imagePromises)
+		.then((loadedImages)=>{
+			loadedImages.forEach((loadedImg)=>{
+				document.getElementById("output").appendChild(loadImage)
+			})
+		})
+		.catch(error){
+			console.error(`Failed to load image's URL: ${error}`)
+		}
+}
+
+btn.addEventListener('click', displayImages);
